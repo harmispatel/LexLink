45a548686fee262b9593c48a751e653dfe6ad16a
Harmis Technology <harmistest@gmail.com>
2017-03-14 16:07:03 +0530

    Home content added

---
+++ app/src/main/java/lexlink/app/com/lexlink/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package lexlink.app.com.lexlink.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lexlink.app.com.lexlink.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
