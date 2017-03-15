1bd54597b62b928341377ed14c9f6cc702b7b02c
Harmis Technology <harmistest@gmail.com>
2017-03-15 11:04:58 +0530

    gradle file changed and json keyword added

---
+++ app/src/main/java/lexlink/app/com/lexlink/jsonutil/JSONCommonKeywords.java
@@ -0,0 +1,27 @@
+package lexlink.app.com.lexlink.jsonutil;
+
+
+public class JSONCommonKeywords {
+
+
+    //Register and Login
+    public static String success = "success";
+    public static String Profile = "Profile";
+    public static String userId = "userId";
+    public static String Salutation = "Salutation";
+    public static String FirstName = "FirstName";
+    public static String MiddleName = "MiddleName";
+    public static String LastName = "LastName";
+    public static String Email = "Email";
+    public static String LoginType = "LoginType";
+
+    public static String UserName = "UserName";
+    public static String PassWord = "PassWord";
+    public static String isSocial = "isSocial";
+
+    public static String DeviceToken = "PassWord";
+    public static String DeviceAccess = "PassWord";
+    public static String profile_pic = "PassWord";
+
+
+}
